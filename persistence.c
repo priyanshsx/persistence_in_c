ff47cf081c4c20a36c33d5b5476cb5fb76214d22
Priyansh <saxenapriyansh95@gmail.com>
2026-09-15 10:14:07 +0530

    added the structure for the program

--- persistence.c
+++ persistence.c
@@ -1,2 +1,14 @@
+// importing libraries
+
 #include<stdio.h> 
 #include<stdlib.h>
+
+
+// ask the user for an input string 
+
+// read into the file on computer and write some text to it 
+
+// save the file 
+
+// close the file 
+
