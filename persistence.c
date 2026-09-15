2c20a1e2325f5b2a28b13dff0d7cf1926830ac3c
Priyansh <saxenapriyansh95@gmail.com>
2026-09-15 10:17:57 +0530

    updated the structure of the program

--- persistence.c
+++ persistence.c
@@ -2,13 +2,37 @@
 
 #include<stdio.h> 
 #include<stdlib.h>
+ 
+int main() {
+
+    // create the storage array
+    
+    char log_array[200];
 
 
-// ask the user for an input string 
 
-// read into the file on computer and write some text to it 
+}
+
+
+// ask the user for an input string (fgets(where to save, max size, where to read input from))
+
+
+
+// open the file 
+
+
+
+    // ensure to check that the pointer actually opened the file 
+
+
+
+// write to the file  
+
+
 
 // save the file 
+
+
 
 // close the file 
 
