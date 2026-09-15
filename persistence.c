08625899b2a078c402c5b80bf4b27054e5915df7
Priyansh <saxenapriyansh95@gmail.com>
2026-09-15 10:28:06 +0530

    ran, debugged, and fixed errors

--- persistence.c
+++ persistence.c
@@ -9,30 +9,33 @@ int main() {
     
     char log_array[200];
 
+    // ask the user for an input string and save using fgets()
 
+    printf("Enter a string: ");
+
+    fgets(log_array, sizeof(log_array), stdin);
+
+    // creating a pointer that opens the file 
+
+    FILE *file_pointer = fopen("system_log.txt", "a");
+
+    // ensuring that the pointer actually leads to a file 
+
+    if (file_pointer == NULL) {
+        printf("Fatal error: couldn't create file. Please try the program again.");
+        return 1;
+    }
+
+    // writing to the file 
+
+    fprintf(file_pointer, "Input from user: %s", log_array);
+
+    // closing the file to free memory 
+
+    fclose(file_pointer);
+
+    printf("Data appended successfully.\n");
+
+    return 0;
 
 }
-
-
-// ask the user for an input string (fgets(where to save, max size, where to read input from))
-
-
-
-// open the file 
-
-
-
-    // ensure to check that the pointer actually opened the file 
-
-
-
-// write to the file  
-
-
-
-// save the file 
-
-
-
-// close the file 
-
